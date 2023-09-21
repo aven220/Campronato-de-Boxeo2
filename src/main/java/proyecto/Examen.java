df3350ecb145feedd449f31dd530f3d37adf2b5e
pc_Gamer <pc_Gamer@DESKTOP-KHNF6IQ.mshome.net>
2023-09-21 13:32:03 -0500

    Revert "version 0.2"
    This reverts commit b087d4a66f04c82b964a062201a2aae20de13369.

--- src/main/java/proyecto/Examen.java
+++ src/main/java/proyecto/Examen.java
@@ -9,4 +9,25 @@ private boolean antidoping;
 
     public Examen() {
     }
+
+    public Examen(double pesaje, boolean antidoping) {
+        this.pesaje = pesaje;
+        this.antidoping = antidoping;
+    }
+
+    public double getPesaje() {
+        return pesaje;
+    }
+
+    public void setPesaje(double pesaje) {
+        this.pesaje = pesaje;
+    }
+
+    public boolean isAntidoping() {
+        return antidoping;
+    }
+
+    public void setAntidoping(boolean antidoping) {
+        this.antidoping = antidoping;
+    }
 }
